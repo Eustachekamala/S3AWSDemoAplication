cd8fc14697cbc959900609ab161ae4efcb226779
eustachekamala <eustachejared25@gmail.com>
2026-02-03 14:42:16 +0300

    1. Initial Commit 2. Implemented the s3Bucket functionnality with the aws sdk

---
+++ src/main/java/com/eustache/S3DemoApplication/controllers/S3Controller.java
@@ -0,0 +1,35 @@
+package com.eustache.S3DemoApplication.controllers;
+
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.eustache.S3DemoApplication.services.S3Service;
+
+@RestController
+public class S3Controller {
+    @Autowired
+    private S3Service s3Service;
+
+    @PostMapping("/upload")
+    public ResponseEntity<String> upload(@RequestParam("file") MultipartFile file) throws IOException {
+        s3Service.uploadFile(file);
+        return ResponseEntity.ok("File uploaded successfully");
+    }
+
+    @GetMapping("/download/{fileName}")
+    public ResponseEntity<byte[]> download(@PathVariable String fileName) {
+        byte[] data = s3Service.downloadFile(fileName);
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + fileName + "\"")
+                .body(data);
+    }
+}
