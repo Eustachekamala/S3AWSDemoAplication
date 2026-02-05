eeed81b963da2956d1ca6047ed93eef57fd3028c
eustachekamala <eustachejared25@gmail.com>
2026-02-05 10:54:05 +0300

    1. created two applications yaml, one for local purpose and an other one for dev purpose. 2. Updated the S3Client classes inside the S3Config file to use the profile either dev or local

--- src/main/java/com/eustache/S3DemoApplication/config/S3Config.java
+++ src/main/java/com/eustache/S3DemoApplication/config/S3Config.java
@@ -3,8 +3,10 @@ package com.eustache.S3DemoApplication.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
@@ -22,12 +24,22 @@ public class S3Config {
     private String region;
 
 
-    @Bean
+    @Bean("s3Client")
+    @Profile("local")
     public S3Client s3Client(){
         AwsBasicCredentials awsBasicCredentials = AwsBasicCredentials.create(accessKey, secretKey);
         return S3Client.builder()
             .region(Region.of(region))
             .credentialsProvider(StaticCredentialsProvider.create(awsBasicCredentials))
+            .build();
+    }
+
+    @Bean("s3Client")
+    @Profile("dev")
+    public S3Client s3ClientDev(){
+        return S3Client.builder()
+            .region(Region.of(region))
+            .credentialsProvider(DefaultCredentialsProvider.create())
             .build();
     }
 }
